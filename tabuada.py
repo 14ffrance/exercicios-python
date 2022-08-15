56608a3e9439cda562f02a0ec96aaf56572daac1
Alexandre França <af328740@gmail.com>
2022-08-15 14:00:29 -0300

    Tabuada
    Digite um número para vê a sua tabuada.

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+print ('='*15)
+tabu = int (input ('Digite um número para ver a tabuada: '))
+print ('   TABUADA')
+n1 = tabu * 1
+n2 = tabu * 2
+n3 = tabu * 3
+n4 = tabu * 4
+n5 = tabu * 5
+n6 = tabu * 6
+n7 = tabu * 7
+n8 = tabu * 8
+n9 = tabu * 9
+n10 = tabu * 10 
+print(f' {tabu} x 1 = {n1}\n {tabu} x 2 = {n2}\n {tabu} x 3 = {n3}\n {tabu} x 4 = {n4}\n {tabu} x 5 = {n5}\n {tabu} x 6 = {n6}\n {tabu} x 7 = {n7}\n {tabu} x 8 = {n8}\n {tabu} x 9 = {n9}\n {tabu} x 10 = {n10}')
+print ('='*15)
