40adf60df343728b98895625d5121370d45c5adc
Alexandre França <af328740@gmail.com>
2022-08-15 14:29:23 -0300

    Dólar e Euro
    O sistema converte o Real para Dólar e Euro

---
+++ dolar e euro.py
@@ -0,0 +1,5 @@
+print ('Seja bem-vindo! Ao conversor de reais para dólares e euros!')
+real = float (input('Quanto você tem na carteira? R$ '))
+calc1 = real / 5.08
+calc2 = real / 5.18
+print (f'Você poderá comprar U$ {calc1:.2f} dólares! E € {calc2:.2f} euros! ')
