5688ddb37cd1229a0144e2f25f223e4f94f41220
Alexandre França <af328740@gmail.com>
2022-08-15 14:10:22 -0300

    Aritméticos
    O programa vai amostrar a soma, subtração, divisão normal, multiplicação e a divisão inteira.

---
+++ aritimeticos.py
@@ -0,0 +1,8 @@
+n1 = int (input('Digite um valor: '))
+n2 = int (input ('Digite outro valor: '))
+s = n1+n2
+sub = n1-n2
+d = n1/n2 
+m = n1*n2
+di = n1//n2
+print (f'A soma vale {s}, a subtração vale {sub},\nE a divisão normal vale {d}, a multiplicação vale {m} e a divisão inteira vale {di}')
