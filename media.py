8e8395f77b8d0899dccbeb8406495737a222cfa2
Alexandre França <af328740@gmail.com>
2022-08-17 19:50:19 -0300

    Create media.py
    Enter the student's first grade and the second, then it will give the student's average.

---
+++ media.py
@@ -0,0 +1,5 @@
+nome = (input('Nome do aluno (a): '))
+num1 = float (input('Digite a primeira nota: '))
+num2 = float (input('Digite a segunda nota: '))
+nota1 = (num1 + num2) / 2
+print (f'A sua média do {nome} é {nota1:.2f}')
