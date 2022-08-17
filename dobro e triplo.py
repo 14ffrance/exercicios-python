c50249e4d44a5bdfc724dd29fd3bf8af35a282dc
Alexandre França <af328740@gmail.com>
2022-08-17 18:35:08 -0300

    Update dobro e triplo.py

--- dobro e triplo.py
+++ dobro e triplo.py
@@ -1,6 +1,5 @@
-num = int (input ('Digite um valor: '))
+num = int (input ('Digite um valor para ver o dobro, triplo e a raiz: '))
 dobro = num * 2
 triplo = num * 3
 raiz = num ** (1/2)
 print (f'O dobro de {num} é {dobro} e o triplo é {triplo} \nA raiz é {raiz:.2f}')
- 
