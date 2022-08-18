2bb488166122d31c1ab85d45377347712a0d7900
Alexandre França <af328740@gmail.com>
2022-08-18 19:35:09 -0300

    Update media.py

--- media.py
+++ media.py
@@ -2,4 +2,4 @@ nome = (input('Nome do aluno (a): '))
 num1 = float (input('Digite a primeira nota: '))
 num2 = float (input('Digite a segunda nota: '))
 nota1 = (num1 + num2) / 2
-print (f'A sua média do {nome} é {nota1:.2f}')
+print (f'A sua média do {nome} é {nota1:.1f}')
